9aaf484a8f53b77e43c937c396763614e92bfb44
llsh <158769991+llshdev@users.noreply.github.com>
2026-01-22 15:40:51 +0300

    Modify ExampleMixin to limit player pitch
    Change mixin target from MinecraftServer to ClientPlayerEntity and add pitch limiting functionality.

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,19 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(ClientPlayerEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void limitPitch(CallbackInfo info) {
+        ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
+        // Если смотрим вниз больше чем на 87 градусов - ставим ровно 87
+        if (player.getPitch() > 87.0f) {
+            player.setPitch(87.0f);
+        }
+    }
 }
